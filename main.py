dea71aa17a09a2207a659bd5c8b2500b20ec5eb0
arnab123 <arnab.kolkata98@gmail.com>
2022-04-22 23:23:17 +0530

    first commit

--- main.py
+++ main.py
@@ -35,7 +35,8 @@ def predictRoute():
 
 clApp = ClientApp()
 # #port = int(os.getenv("PORT"))
-if __name__ == "__main__":
-    # clApp = ClientApp()
-    # app.run(host='0.0.0.0', port=port)
-    app.run(debug=True)
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. You
+    # can configure startup instructions by adding `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
